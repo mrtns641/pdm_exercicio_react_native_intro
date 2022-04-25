3e13c068cb1c6faf39766a675e39aafe97b7fb80
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-04-25 01:57:14 -0300

    Gerador de Números aleatórios desenvolvido

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const geraNumeros = () => {
+  var listaNumeros = [];
+
+  for (let i = 0; i < 6; i++) {
+    let numeroAleatorio = Math.floor(Math.random() * 60 + 1);
+      if (listaNumeros.indexOf(numeroAleatorio) === -1) {
+        listaNumeros.push(numeroAleatorio);
+      }
+  }
+  return listaNumeros;
+}
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      texto: 'Gerador de Números',
+      retorno: geraNumeros()
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.texto}</Text>
+        <Text>{ this.state.retorno[0] + " " + 
+                this.state.retorno[1] + " " + 
+                this.state.retorno[2]+  " " + 
+                this.state.retorno[3]+  " " + 
+                this.state.retorno[4]+  " " + 
+                this.state.retorno[5]}
+        </Text>
+        <Button
+          title='Clique Aqui'
+          onPress={() => {this.setState({
+            retorno: this.retorno = geraNumeros()
+          })}}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
